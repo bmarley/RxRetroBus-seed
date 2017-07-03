3a7fd26378fdc2b940e28ad99511eaa6fbb8686e
Eric Su <esu@credera.com>
2017-07-03 13:23:01 -0500

    Improve messaging
    - Show toast with success and failure messages upon response
    - Remove exampleGet call in onCreate

--- app/src/main/java/com/blarley/rxretrobusseed/activities/MainActivity.java
+++ app/src/main/java/com/blarley/rxretrobusseed/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.blarley.rxretrobusseed.library.bus.RetroSubscriber;
 import com.blarley.rxretrobusseed.App;
@@ -25,11 +26,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onSuccess(ExampleGetModel response) {
             findViewById(R.id.progressBar).setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this, "Success: " + response.getExampleField(), Toast.LENGTH_LONG).show();
             Log.d("test", response.toString());
         }
 
         @Override
         public void onError(Throwable throwable) {
+            findViewById(R.id.progressBar).setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this, "Error: " + throwable.getMessage(), Toast.LENGTH_LONG).show();
             Log.d("test", throwable.getMessage());
         }
     };
@@ -38,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        App.clients.ExampleGet.exampleGet();
         findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
